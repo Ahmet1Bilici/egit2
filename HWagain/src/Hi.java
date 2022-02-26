7f178d344424efc38ef7a700b4b1b7f84b5ffba8
Ahmet <1yigitbilici@gmail.com>
2022-02-26 12:40:18 -0600

    first commit what d u expect

---
+++ HWagain/src/Hi.java
@@ -0,0 +1,10 @@
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yo me again we re practicing things as usual");
+
+	}
+
+}
