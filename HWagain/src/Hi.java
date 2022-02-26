cd597d8d85df4bb641883f407242907d39a991ba
Ahmet <1yigitbilici@gmail.com>
2022-02-26 12:41:18 -0600

    me again

--- HWagain/src/Hi.java
+++ HWagain/src/Hi.java
@@ -4,6 +4,7 @@ public class Hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Yo me again we re practicing things as usual");
+		System.out.println("still testing some stuff");
 
 	}
 
